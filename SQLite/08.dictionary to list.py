3b94eb454498785b332e37b0da1e2ddf552c3187
Bruce Ashbee - laptop <a15714t@reitaku.jp>
2020-12-09 14:12:13 +0800

    after python class

---
+++ SQLite/08.dictionary to list.py
@@ -0,0 +1,7 @@
+dic={"A":"APPLE","B":"Boy"}
+for key in dic:
+    print(dic[key])
+
+a = 1
+if isinstance(a, int) == True:
+    print(a, " is a integer")
---
+++ SQLite/08.teacher's code.py
@@ -0,0 +1,42 @@
+import requests
+import sqlite3
+def execSQLcommand(sqlstr):
+    global myConn
+    global myCursor
+    try:
+      myCursor = myConn.execute(sqlstr)
+      myConn.commit()
+      print("指令已成功執行:" + sqlstr)
+      return myCursor
+    except:
+      print("指令有誤:", sqlstr)
+def qo(instr):
+    return "'" + instr + "'"
+myConn = sqlite3.connect(r"C:\Users\Bruce Ashbee\Documents\Python\SQLite\std.db")
+myCursor = myConn.cursor()
+
+ 
+
+websource = requests.get('https://datacenter.taichung.gov.tw/swagger/OpenData/e0c55c8f-1c74-47ce-909c-8ecacbfaaeb7')
+#print("websource:", type(websource)) #<class 'requests.models.Response'>
+myJson = websource.json()
+#print(type(myJson)) #myJson is a list
+#sqlstr = 'delete from test'
+#execSQLcommand(sqlstr)
+for idx , content in enumerate(myJson,0): 
+    print("====================")
+    print("idx={0}, content={1}".format(idx, content))
+    #print("content type:", type(content)) #content is a dictionary
+    print("---")
+    
+    sqlstr = " insert into test(conuty_name, county_id, family_Num, religion, profession, Voluntary, rd, val_year) values("
+    for key in content:
+        #print("key:", key, "value:", content[key] )
+        sqlstr += qo(content[key]) + "," 
+    sqlstr = sqlstr[0:-1] + ")"
+    execSQLcommand(sqlstr)
+
+ 
+
+    print(sqlstr)
+ 
