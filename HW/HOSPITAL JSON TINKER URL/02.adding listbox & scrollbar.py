fdaab2080bf52e65f36166e212f515371bc09c02
Bruce Ashbee - Destop <a15714t@reitaku.jp>
2020-12-16 16:09:32 +0800

    add selections to list successfully

--- HW/HOSPITAL JSON TINKER URL/02.adding listbox & scrollbar.py
+++ HW/HOSPITAL JSON TINKER URL/02.adding listbox & scrollbar.py
@@ -27,27 +27,27 @@ URL_entry = Entry(root,width=100,bg="#353130",fg="white") #! height=1  cant
 #URL_entry.config()
 URL_entry.grid(row=1,column=1)
 #! WIN DESTOP
-#conn = sqlite3.connect('C:\\Users\\Bruce Ashbee\\Documents\\Python\\HW\\HOSPITAL JSON TINKER URL\\hospital.db')
+conn = sqlite3.connect('C:\\Users\\Bruce Ashbee\\Documents\\Python\\HW\\HOSPITAL JSON TINKER URL\\hospital.db')
 #! MAC
-
-conn = sqlite3.connect('//Users//bruceashbee//Documents//Python//HW//HOSPITAL JSON TINKER URL//hospital.db')
+#conn = sqlite3.connect('//Users//bruceashbee//Documents//Python//HW//HOSPITAL JSON TINKER URL//hospital.db')
 def execute_SQL_command(sql_command):
     #! global sql_command
     #! SyntaxError: name 'sql_command' is parameter and global
     global conn
-    print(sql_command)   #* for double checking each sql command
+    print(sql_command)   #? for double checking each sql command
     conn.executescript(sql_command)
     conn.commit() 
     #conn.close() #! if u leave it here then u will only permit to insert 1 data only
                   #! database is locked
 def listbox_add_selection():
-    global value
-    my_listbox.insert(END,value[1])
+    global content
+    #print(content['醫院名稱'])#? check for getting values for adding listbox selections
+    my_listbox.insert(END,content['醫院名稱'])
 
 
 
 def get_JSON_frm_internet():
-    global value
+    global content
     #?とりあえず例として、どこかのWeb APIを叩くことにする
     url = URL_entry.get()
     #?requests.getを使うと、レスポンス内容を取得できるのでとりあえず変数へ保存
@@ -64,6 +64,7 @@ def get_JSON_frm_internet():
     #* jsonData is <class 'list'>
     for idx , content in enumerate(jsonData,0): 
         #print(content)
+        listbox_add_selection()
         ##{'所在縣市': '澎湖縣', '醫院名稱': '三軍總醫院澎湖分院附設民眾診療服務處', '編號': '1', '醫院評鑑評鑑結果': '醫院評鑑合格（地區醫院）', '教學醫院評鑑結果': '醫師及醫事人員類教學醫院評鑑合格', '醫院評鑑合格效期': '106/1/1-109/12/31', '教學醫院合格效期': '106/1/1-109/12/31', '醫院電話': '06-9211116', '地址': '澎湖縣馬公市前寮里90號1-5樓'}  
         #print("====================")
         #print("idx={0}, content={1} \n".format(idx, content))
@@ -75,7 +76,7 @@ def get_JSON_frm_internet():
         #print(type(content_values))
         ## <class 'dict_values'>
 
-        listbox_add_selection()
+        
         sql_command = " insert into Hospital (City, Hospital, H_Id, Eval_Reault_Local, Eval_Reault_Teaching, Date_Valid_Local, Date_Valid_Teaching, Tel, Address) values("
         for value in content_values:
             #if isinstance(value,str) != True:
@@ -107,11 +108,11 @@ get_JSON_frm_internet_btn.grid(row=1,column=2)
 
 #add Frame
 my_frame = Frame(root)
-my_frame.grid(row=2,column=0,columnspan=3,sticky=W+E)
+my_frame.grid(row=2,column=0,columnspan=2,sticky=W+E)
 
 #scroll bar
 scroll_bar_4_my_listbox = Scrollbar(my_frame,orient=VERTICAL)
-scroll_bar_4_my_listbox.grid(row=2,column=1,sticky=N+S)
+scroll_bar_4_my_listbox.grid(row=2,column=1,sticky=N+S+W)
 
 #Listbox
 #*  SELECT MODE = SINGLE IS AT DEFAULT
