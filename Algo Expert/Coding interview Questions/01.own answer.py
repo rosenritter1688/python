713a76099079e481eb584d912f079f3e9a3c671c
Bruce Ashbee <a15714t@reitaku.jp>
2021-09-06 00:06:07 +0900

    Algo Expert interview question 1

---
+++ Algo Expert/Coding interview Questions/01.own answer.py
@@ -0,0 +1,18 @@
+## array = [3,5,-4,8,11,1,-1,-6]
+## TargetSum = 10
+
+
+array = [3,5,-4,8,11,1,-1,-6]
+TargetSum = 10
+
+numbers_of_elements = len(array)
+print(numbers_of_elements)
+
+'''##! FYI counts how many elements in a list
+counter = 0
+for index in array:
+    counter += 1
+print("counter = " + str(counter))
+'''
+
+
